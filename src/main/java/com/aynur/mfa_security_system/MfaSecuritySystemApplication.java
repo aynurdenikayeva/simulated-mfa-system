f790d31ce5fbb19206cb26d23e4e9540bf180377
Aynur <113580226+aynurdenikayeva@users.noreply.github.com>
2026-05-16 21:58:58 +0400

    first commit

---
+++ src/main/java/com/aynur/mfa_security_system/MfaSecuritySystemApplication.java
@@ -0,0 +1,13 @@
+package com.aynur.mfa_security_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfaSecuritySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MfaSecuritySystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aynur/mfa_security_system/MfaSecuritySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aynur.mfa_security_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MfaSecuritySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
